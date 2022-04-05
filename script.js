c3317940611d6d4805f74b66cdf2ce86c5f78600
Steven Omole-Adebomi <omosteven123@gmail.com>
2022-04-05 21:27:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const newMember = document.getElementById('login_form')
     existingMember = document.getElementById('registeration_form');
 
-    function registerNow() {
+    function registerNow(e) {
+        e.preventDefault();
         newMember.style.display = "none";
         existingMember.style.display = "block";
     }
 
-    function loginNow () {
+    function loginNow (e) {
+         e.preventDefault();
         existingMember.style.display = "none";
         newMember.style.display = "block";
     }

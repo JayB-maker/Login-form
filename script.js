3f9d6125ca23dc6f64a0badaa1d50d866c2fc725
JayB <ajiboyeabiodun001@gmail.com>
2022-04-07 03:01:56 +0200

    .1

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const newMember = document.getElementById('login_form')
     existingMember = document.getElementById('registeration_form');
 
-    function registerNow() {
+    function registerNow(e) {
+        e.preventdefault();
         newMember.style.display = "none";
         existingMember.style.display = "block";
     }
 
-    function loginNow () {
+    function loginNow (e) {
+        e.preventdefault();
         existingMember.style.display = "none";
         newMember.style.display = "block";
     }
